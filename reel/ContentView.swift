4e3a5a9d308591f421594dec5e09e3727d8001e9
Francesco Paciello <kekkopacio@gmail.com>
2021-04-21 19:29:30 +0200

    Initial Commit

---
+++ reel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  reel
+//
+//  Created by Kekko Paciello on 21/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ reel/reelApp.swift
@@ -0,0 +1,17 @@
+//
+//  reelApp.swift
+//  reel
+//
+//  Created by Kekko Paciello on 21/04/21.
+//
+
+import SwiftUI
+
+@main
+struct reelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
